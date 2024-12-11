cd1b2e289a4861be4213c0c8549e080d45780dc0
Matthew <matthew@nevadacloud.com>
2024-12-11 13:03:28 +0200

    Changed it to serving for over 75 years

--- src/app/pages/HomePage.jsx
+++ src/app/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <div className="w-full h-[600px] bg-[url('https://s3.eu-west-2.amazonaws.com/luminablue-blogs/1721909136_67_banner.png')] bg-cover bg-center text-center text-white">
       <div className="bg-black bg-opacity-50 h-full flex flex-col items-center justify-center p-4">
         <p className="text-3xl mb-8">
-          Serving the community for over 60 years delivering the highest quality care and products for our customers
+          Serving the community for over 75 years delivering the highest quality care and products for our customers
         </p>
         <button className="px-6 py-3 bg-primary text-white font-semibold rounded-lg hover:bg-white hover:text-primary hover:border-orange-500 border-1 transition">
           Make A Booking
